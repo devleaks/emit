c804809711703815e5210e88fe06e4430cae222c
Pierre Mareschal <devleaks.be@gmail.com>
2020-02-12 12:08:16 +0100

    Added flightboard skeleton. backup before all-turfjs

---
+++ flightboard.js
@@ -0,0 +1,37 @@
+const fs = require('fs')
+const moment = require('moment')
+const parse = require('csv-parse/lib/sync')
+
+var program = require('commander')
+
+const geojson = require('./geojson-util')
+const debug = require('./debug.js')
+
+
+program
+    .version('1.0.0')
+    .description('generates flights from flight board (departure and arrival)')
+    .option('-d, --debug', 'output extra debugging')
+    .requiredOption('-f, --flightboard <file>', 'CSV flightboard')
+    .option('-o, --output <file>', 'Save to file, default to out.json', "out.json")
+    .parse(process.argv)
+
+debug.init(program.debug, [""], "main")
+debug.print(program.opts())
+
+var airport = {}
+
+// list.sort((a, b) => (a.isodatetime > b.isodatetime) ? 1 : -1)
+
+var features = []
+
+if (program.flightboard) {
+    const csvstring = fs.readFileSync(program.flightboard, 'utf8')
+    const records = parse(csvstring, {columns: true})
+    records.forEach(function(s,idx) {
+      debug.print(s)
+    })
+}
+
+fs.writeFileSync(program.output, JSON.stringify(geojson.FeatureCollection(features)), { mode: 0o644 })
+debug.print(program.output + ' written')
