986599b1f2fc5248c94a3e912f51ede2ab9562dc
Pierre Mareschal <devleaks.be@gmail.com>
2020-02-25 09:02:54 +0100

    changed -s speed/silent to -q quiet

--- emit.js
+++ emit.js
@@ -16,7 +16,7 @@ program
     .option('-a, --altitude', 'Add altitude to GeoJSON positions')
     .option('-r, --rate <rate>', 'Rate of event report in seconds, default 30 s', 30)
     .option('-j, --jitter <distance>', 'GPS precision in meter', 0)
-    .option('-s, --silent', 'Does not report position when stopped')
+    .option('-q, --quiet', 'Does not report position when stopped')
     .option('--min-speed <speed>', 'Minimum speed for objects (km/h)', 5)
     .option('-v, --vertices', 'Emit event at vertices (change of direction)')
     .option('-l, --last-point', 'Emit event at last point of line string, even if time rate is not elapsed')
