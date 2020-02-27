f6d2dc17c178dbebf905fda98d0d47a3e64e076e
Pierre Mareschal <devleaks.be@gmail.com>
2020-02-27 08:58:30 +0100

    shell script to chain stuff

---
+++ sim.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+COUNT=0
+CLEAN="NO"
+POSITIONAL=()
+
+while [[ $# -gt 0 ]]
+do
+    key="$1"
+    case $key in
+        -c|--count)
+        COUNT="$2"
+        shift # past argument
+        shift # past value
+        ;;
+        --clean)
+        CLEAN="YES"
+        shift # past argument
+        ;;
+        *)    # unknown option
+        POSITIONAL+=("$1") # save it in an array for later
+        shift # past argument
+        ;;
+    esac
+done
+
+set -- "${POSITIONAL[@]}" # restore positional parameters
+if [ $COUNT -gt 0 ]
+then   
+    tempfile=fb$$.csv
+    node mkfb.js -c $COUNT -o $tempfile
+    node flightboard.js -f $tempfile
+    rm -f $tempfile
+    cat FLIGHT-*.csv SERVICE-*.csv | sort -t , -k 3 -o out.csv
+    ls -l out.csv
+fi
+
+if [ $CLEAN = "YES" ]
+  then rm -f FLIGHT-*.{csv,json} SERVICE-*.{csv,json}
+fi
