156809264a00b84aef52df8444c613955b58eab0
Pierre Mareschal <devleaks.be@gmail.com>
2020-10-02 16:21:37 +0200

    changed to kafkajs rather than kafka-node (too big)

--- src/lib/kafka-post-lib.js
+++ src/lib/kafka-post-lib.js
@@ -1,42 +1,30 @@
-import * as debug from "./debug.js";
-import * as config from "../data/kafka-config.js";
+import config from "../data/kafka-config.js";
+/* Kafka config:
 
-import pkg from "kafka-node";
-const { Producer, KafkaClient } = pkg;
+export default {
+    clientId: "gip-simulator",
+    brokers: ["kafkahostname:9092"]
+};
 
+*/
+import KafkaImport from "kafkajs";
+const { Kafka } = KafkaImport;
 
 let client
 let producer
 
 export const init = function() {
-    client = new KafkaClient(config.kafka_server);
-    producer = new Producer(client);
+    console.log(config)
+    client = new Kafka(config)
+    producer = client.producer()
 }
 
-export const send = function(record) {
-    try {
-        const kafka_topic = record.queue;
-        let payloads = [{
-            topic: kafka_topic,
-            messages: record
-        }];
-
-        producer.on("ready", async function() {
-            let push_status = producer.send(payloads, (err, data) => {
-                if (err) {
-                    debug.print("[kafka-producer -> " + kafka_topic + "]: broker update failed");
-                } else {
-                    debug.print("[kafka-producer -> " + kafka_topic + "]: broker update success");
-                }
-            });
-        });
-
-        producer.on("error", function(err) {
-            debug.print(err);
-            debug.print("[kafka-producer -> " + kafka_topic + "]: connection errored");
-            throw err;
-        });
-    } catch (e) {
-        debug.print(e);
-    }
+export const send = async function(record) {
+    await producer.connect()
+    await producer.send({
+        topic: record.queue,
+        messages: [
+            record
+        ],
+    })
 }
