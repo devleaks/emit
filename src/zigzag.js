45a4e1fb4008be40e8750545c83f8989095733bd
Pierre Mareschal <devleaks.be@gmail.com>
2020-10-02 09:14:54 +0200

    Changed options for commander

--- src/zigzag.js
+++ src/zigzag.js
@@ -16,7 +16,7 @@ program
     .version("1.1.0")
     .description("generates a LineString Feature of zigzag movements")
     .option("-d, --debug", "output extra debugging")
-    .option("-o <file>, --output <file>", "Save to file, default to out.json", "zigzag.json")
+    .option("-o, --output <file>", "Save to file, default to out.json", "zigzag.json")
     .requiredOption("-l, --length <length>", "Length of segment")
     .requiredOption("-c, --count <count>", "Count of zigzags")
     .parse(process.argv)
@@ -26,5 +26,5 @@ debug.print(program.opts())
 const count = parseInt(program.count)
 const length = parseInt(program.length) // km
 
-fs.writeFileSync(program.O, JSON.stringify(zigzag.zigzag([4.3483286,50.8445793],length,count,program)), { mode: 0o644 })
-debug.print(program.O + " written")
+fs.writeFileSync(program.output, JSON.stringify(zigzag.zigzag([4.3483286,50.8445793],length,count,program)), { mode: 0o644 })
+debug.print(program.output + " written")
