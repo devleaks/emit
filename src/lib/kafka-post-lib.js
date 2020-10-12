b9d5034e4d2b95789ac8520f0edcf765b9433b97
Pierre Mareschal <devleaks.be@gmail.com>
2020-10-12 12:17:18 +0200

    Adjusted for kafka topic posts

--- src/lib/kafka-post-lib.js
+++ src/lib/kafka-post-lib.js
@@ -6,26 +6,37 @@ export default {
     clientId: "gip-simulator",
     brokers: ["kafkahostname:9092"]
 };
-
 */
 import KafkaImport from "kafkajs";
 const { Kafka } = KafkaImport;
 
+
+// comment out to send message to GIP queues
+config.TEST_TOPIC = "test"
 let producer
 
 export const init = function() {
     debug.print(config)
-    let client = new Kafka(config)
-    producer = client.producer()
+    const kafka = new Kafka({
+        brokers: ["ordralfabetix.gip4.com:9192"],
+        clientId: "emit",
+    })
+    producer = kafka.producer()
 }
 
-export const send = async function(data) {
-    const dest = JSON.parse(data)
-    await producer.connect()
-    await producer.send({
-        topic: dest.type,
-        messages: [
-            data
-        ],
+export const send = function(data) {
+    const dest = JSON.parse(data) // dest.type is the topic
+    producer.connect().then(() => {
+        debug.print("kpost::send: sending..")
+        const topic = config.TEST_TOPIC ? config.TEST_TOPIC : dest.type
+        producer.send({
+            topic,
+            messages: [{
+                key: dest.type,
+                value: data
+            }],
+        })
+        .then(debug.print("kpost::send: ..sent"))
+        .catch(e => console.error(`kpost::send: ${e.message}`, e))
     })
 }
